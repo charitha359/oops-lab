04034cc61936602dab039c250858e0dd91882850
charitha359 <charithamettupalle@gmail.com>
2025-03-12 19:33:53 +0530

    Add files via upload

---
+++ Program_Files/Exercise-3/LargestNumber.java
@@ -0,0 +1,13 @@
+public class LargestNumber {
+    public static void main(String[] args) {
+        int a = 10, b = 20, c = 15;
+
+        if (a > b && a > c) {
+            System.out.println("Largest: " + a);
+        } else if (b > a && b > c) {
+            System.out.println("Largest: " + b);
+        } else {
+            System.out.println("Largest: " + c);
+        }
+    }
+}
